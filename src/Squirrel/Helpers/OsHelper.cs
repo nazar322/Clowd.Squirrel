bda567cffb1f94da4e63c84c7ebaa2dfe5afa752
Nazar Grynko <nazar.grynko@gmail.com>
2023-01-03 16:46:00 +0200

    OsHelper added

---
+++ src/Squirrel/Helpers/OsHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Squirrel
+{
+    public class OsHelper
+    {
+        public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        public static bool IsOsx => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static string ExecuteCommand(string cmd)
+        {
+            string exeName;
+
+            if (IsWindows)
+            {
+                exeName = "cmd.exe";
+                cmd = "/C " + cmd;
+            }
+            else
+            {
+                exeName = "bash";
+                cmd = $"-c \"{cmd}\"";
+            }
+
+            var psi = new ProcessStartInfo(exeName, cmd) {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            var console = Process.Start(psi);
+            var output = console.StandardOutput.ReadToEnd();
+            console.WaitForExit();
+
+            return output;
+        }
+    }
+}
